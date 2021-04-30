ea08da6d87318755138a269f896a34a7afc5b23d
darkskoll <erick.lievana@gmail.com>
2021-04-29 21:01:58 -0500

    Final commit

---
+++ dobles/1nucleo/graficar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+paste promediosMPITiempo.txt promediosOMPTiempo.txt promediosSecTiempo.txt ordenes.txt > promediosTiempo.txt
+paste promediosMPIMemoria.txt promediosOMPMemoria.txt promediosSecMemoria.txt ordenes.txt > promediosMemoria.txt
+
+gnuplot graficaTiempo.ptl
+gnuplot graficaMemoria.ptl
--- dobles/dobles.sh
+++ dobles/dobles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function mpi(){
-  for i in {1..25}
-  do
-    mpirun -np 4 ./foxMPI $1;
-  done
+  #for i in {1..25}
+  #do
+    #mpirun -np 4 ./foxMPI $1;
+  #done
 
   mv datosTiempo.txt tiempoMPI$1.txt
   mv datosMemoria.txt memoriaMPI$1.txt
@@ -25,10 +25,10 @@ function sec(){
 }
 
 function omp(){
-  for i in {1..25}
-  do
-    mpirun -np 4 ./foxOMP $1 0;
-  done
+  #for i in {1..25}
+  #do
+    ##mpirun -np 4 ./foxOMP $1 0;
+  #done
 
   mv datosTiempo.txt tiempoOMP$1.txt
   mv datosMemoria.txt memoriaOMP$1.txt
@@ -36,22 +36,22 @@ function omp(){
   ./lectura memoriaOMP$1.txt tiempoOMP$1.txt promediosOMP
 }
 
-FILE=datosTiempo.txt
-if [[ -f "$FILE" ]]; then
-  rm datosTiempo.txt
-fi
+#FILE=datosTiempo.txt
+#if [[ -f "$FILE" ]]; then
+  #rm datosTiempo.txt
+#fi
+#
+#FILE=datosMemoria.txt
+#if [[ -f "$FILE" ]]; then
+  #rm datosMemoria.txt
+#fi
 
-FILE=datosMemoria.txt
-if [[ -f "$FILE" ]]; then
-  rm datosMemoria.txt
-fi
-
-mpi "64"
-mpi "128"
-mpi "256"
-mpi "512"
-mpi "1024"
-mpi "2048"
+#mpi "64"
+#mpi "128"
+#mpi "256"
+#mpi "512"
+#mpi "1024"
+#mpi "2048"
 #mpi "4096"
 #mpi "8192"
 
@@ -60,15 +60,15 @@ mpi "2048"
 #sec "256"
 #sec "512"
 #sec "1024"
-sec "2048"
+#sec "2048"
 #sec "4096"
 #sec "8192"
 
-omp "64"
-omp "128"
-omp "256"
-omp "512"
-omp "1024"
+#omp "64"
+#omp "128"
+#omp "256"
+#omp "512"
+#omp "1024"
 omp "2048"
 #omp "4096"
 #omp "8192"
